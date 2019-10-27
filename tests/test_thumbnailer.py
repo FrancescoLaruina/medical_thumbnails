4375ebedd42a05ba9b3076bc12a56efb3a74b43c
Francesco Laruina <cescomail@gmail.com>
2019-10-27 16:05:48 +0100

    Initial import.

---
+++ tests/test_thumbnailer.py
@@ -0,0 +1,12 @@
+import SimpleITK as sitk
+from medical_thumbnails import thumbnailer
+
+def test_build_parser():
+    
+    pass
+def test_convert_to_8bit():
+    pass
+def test_load_image():
+    pass
+def test_save_thumbnail():
+    pass
